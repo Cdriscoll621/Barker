7bc9a43fcd6a8380b4866b3dae1de76603663e2f
awberez <alex@berez.com>
2018-02-24 12:06:17 -0600

    very rough routing

--- final/src/App.js
+++ final/src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Header from "./Components/Header/Header";
-import Enter from "./Components/Enter/Enter";
-import Form from "./Components/Form/Form"
+import Form from "./Components/Form/Form";
+import Profile from "./Components/Profile";
 
 class App extends Component {
     render() {
 
         return (
-            <div>
-                <Header>
-                    <p>
-                        Pet Finder
-                    </p>
-                </Header>
-
-                 <Enter>
-
-                    <Form>
-
-                    </Form>
-                 </Enter>
-            </div>
+            <Router>
+                <div>
+                    <Header>
+                        <p>
+                            Pet Finder
+                        </p>
+                    </Header>
+                        <Switch>
+                            <Route exact path="/" component={Form} />
+                            <Route path="/profile/:userId" component={Profile} />
+                        </Switch>
+                </div>
+            </Router>
         );
 
     }
