6a2c9730876b956707e3a283a0903f04a06c3dbf
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2019-08-08 12:52:41 -0500

    Maybe setting up?

---
+++ models/user.js
@@ -0,0 +1,66 @@
+var bcrypt = require ("bcrypt");
+
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define("User", {
+    id: {type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey:true
+    },
+    user_login: {type: DataTypes.STRING,
+      allowNull: false,
+      validate: {isEmail: true
+      }
+    },
+    user_passwd: {type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fname: {type: DataTypes.STRING,
+      aloowNull:true,
+    },
+    lname: {type: DataTypes.STRING,
+      allowNull:true
+    },
+    addr1: {type: DataTypes.STRING,
+      allowNull: true
+    },
+    addr2: {type: DataTypes.STRING,
+      allowNull: true
+    },
+    city: {type: DataTypes.STRING(25),
+      allowNull: true
+    },
+    state: {type: DataTypes.STRING(2),
+      allowNull: true
+    },
+    zip: {type: DataTypes.STRING(10),
+      allowNull: true
+    },
+    owner_profile: {type: DataTypes.STRING,
+      allowNull: true
+    },
+    image: {type: DataTypes.STRING,
+      allowNull: true
+    },
+    geoLocat:{
+      type: DataTypes.GEOMETRY,
+      allowNull: true
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+
+  });
+//   user.beforeCreate((user, options) => {
+//         return bcrypt.hash(user.user_passwd, 10)
+//             .then(hash => {
+//                 user.user_passwd = hash;
+//             })
+//             .catch(err => {
+//                 throw new Error();
+//             });
+//     });
+  /*  user.validate((user,options)=>{
+       return bcrypt.compareSync(user.user_passwd, this.user_passwd)
+   }) */
+  return User;
+};
