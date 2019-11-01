058f7790712186e4947537f5673613533e2b9527
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2019-11-01 11:43:48 -0500

    Got Server Running and Front End running

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if(process.env.JAWSDB_URL){
 }
 else{
   con = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
-    password: "null", //change
+    password: "Marlybear6210_", //change
     database: "doggiePlayDate_db"
   })
 }
@@ -18,5 +18,6 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
 
 module.exports = con;
